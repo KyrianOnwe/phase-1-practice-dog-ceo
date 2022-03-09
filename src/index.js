f185a0ade18fd72cf5d84b4ad9fac8f2433801a5
Kyrian <dr.kyrian.onwe@gmail.com>
2022-03-08 18:04:59 -0600

    commit tree 5669045b553f71051882ddd00f17bebfc352174a

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+let testData = []
+
+function getPics(){
+    fetch (imgUrl)
+    .then(res => res.json())
+    .then(data => writeDataTable(data)) 
+}
+
+function writeDataTable(pic){
+    let listCopy = [pic, ...testData];
+    testData = listCopy[0].message;
+    goodDog()
+}
+
+function putGogPic(dogSnap){
+    let gF = document.createElement('li')
+    gF.className = 'card';
+    gF.innerHTML = `
+    <img src=${dogSnap} />
+    `
+    document.querySelector("#dog-image-container").appendChild(gF);
+}
+
+
+
+function start(){
+    document.addEventListener('DOMContentLoaded', () => {
+        getPics();
+        //console.log(testData)
+        //goodDog();
+    }
+   )
+}
+
+function goodDog(){
+    console.log(testData)
+    testData.map(function (item){
+        return putGogPic(item)
+    })
+}
+
+
+//why doesn't the goodDog work when called from start?
+start()
